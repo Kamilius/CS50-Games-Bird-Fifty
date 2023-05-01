9426f9703e88b3a18314c59077ef463d9dec9a15
Kamilius <kamiklviv@gmail.com>
2023-05-01 09:43:52 +0300

    refactor: assets structure. add: Bird class and assets

--- main.lua
+++ main.lua
@@ -1,4 +1,7 @@
 push = require('push')
+Class = require('class')
+
+require('Bird')
 
 WINDOW_WIDTH = 1280
 WINDOW_HEIGHT = 720
@@ -6,16 +9,18 @@ WINDOW_HEIGHT = 720
 VIRTUAL_WIDTH = 512
 VIRTUAL_HEIGHT = 288
 
-local background = love.graphics.newImage('background.png')
+local background = love.graphics.newImage('assets/images/background.png')
 local backgroundScroll = 0
 
-local ground = love.graphics.newImage('ground.png')
+local ground = love.graphics.newImage('assets/images/ground.png')
 local groundScroll = 0
 
 local BACKGROUND_SCROLL_SPEED = 30
 local GROUND_SCROLL_SPEED = 60
 
 local BACKGROUND_LOOPING_POINT = 413
+
+local bird = Bird()
 
 function love.load()
   love.graphics.setDefaultFilter('nearest', 'nearest')
@@ -55,6 +60,8 @@ function love.draw()
   love.graphics.draw(background, -backgroundScroll, 0)
 
   love.graphics.draw(ground, -groundScroll, VIRTUAL_HEIGHT - 16)
+
+  bird:render()
 
   push:finish()
 end
